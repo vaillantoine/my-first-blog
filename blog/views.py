a0ca5f83a7af50929138617ef5abca76b13e0828
Antoine Vaillant <availlant@ec-m.fr>
2022-11-18 16:59:50 +0100

    15. Finalisation

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
 from django.utils import timezone
 from .models import Billet
+from django.shortcuts import render, get_object_or_404
 
 
 def post_list(request):
     billets = Billet.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'billets': billets})
+
+
+def post_detail(request, pk):
+    billet = get_object_or_404(Billet, pk=pk)
+    return render(request, 'blog/post_detail.html', {'billet': billet})
